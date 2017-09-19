ed407f9f8ff99e5b166511a6c581ad785641b6f5
Prasanna <telepras@gmail.com>
2017-09-19 09:30:19 +0530

    Updates javascript syntax

--- test/users.spec.js
+++ test/users.spec.js
@@ -13,22 +13,28 @@ chai.use(chaiHttp)
 
 // Mock sandbox
 var sandbox = Sinon.sandbox.create()
-
-var genRandomString = function(length){
+  , test_user = {
+  verify_token: null
+}
+  , genRandomString = function(length) {
   return crypto.randomBytes(Math.ceil(length/2))
     .toString('hex') /** convert to hexadecimal format */
-    .slice(0,length);   /** return required number of characters */
-};
-
-var sha512 = function(password, salt){
-  var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-  hash.update(password);
-  var value = hash.digest('hex');
+    .slice(0,length)   /** return required number of characters */
+}
+  , sha512 = function(password, salt) {
+  var hash = crypto.createHmac('sha512', salt) /** Hashing algorithm sha512 */
+  hash.update(password)
+  var value = hash.digest('hex')
   return {
     salt:salt,
     passwordHash:value
-  };
-};
+  }
+}
+  , saltHashPassword = function(userpassword) {
+  var salt = genRandomString(16) /** Gives us salt of length 16 */
+  var passwordData = sha512(userpassword, salt)
+  return passwordData
+}
 
 function saltHashPassword(userpassword) {
   var salt = genRandomString(16); /** Gives us salt of length 16 */
@@ -41,27 +47,27 @@ sandbox.stub(Mailgun({ apiKey: 'foo', domain: 'bar' }).Mailgun.prototype, 'messa
   send: mailgunSendSpy
 })
 
-describe('Users', () => {
-  before((done) => {
+describe('Users', function() {
+  before(function(done) {
     DB.connect(done)
   })
 
-  afterEach((done) => {
+  afterEach(function(done) {
     DB.drop(done)
   })
   // app.post('/signup',user.signup)
-  describe('/POST signup', () => {
+  describe('/POST signup', function() {
 
-    before((done) => {
+    before(function(done) {
       let user = {
         email: "p@example.com"
       }
       users = DB.getDB().collection('users')
       users.insertOne(user)
-        .then(() => done())
+        .then(function() {done()})
     })
 
-    it('should return 404 if user exists', (done) => {
+    it('should return 404 if user exists', function(done) {
       let user = {
         name: 'Person',
         email: 'p@example.com',
@@ -71,7 +77,7 @@ describe('Users', () => {
       chai.request(server)
         .post('/signup')
         .send(user)
-        .end((err, res) => {
+        .end(function(err, res) {
           res.should.have.status(404)
           res.body.should.be.a('object')
           res.body.message.should.equal('User with this email exists')
@@ -79,7 +85,7 @@ describe('Users', () => {
         })
     })
 
-    it('should throw error if email or password is missing', (done) => {
+    it('should throw error if email or password is missing', function(done) {
       let user = {
         email: 'p@example.com'
       }
@@ -87,7 +93,7 @@ describe('Users', () => {
       chai.request(server)
         .post('/signup')
         .send(user)
-        .end((err, res) => {
+        .end(function(err, res) {
           res.should.have.status(422)
           res.body.should.be.a('object')
           res.body.message.should.equal('Email or password missing')
@@ -95,7 +101,7 @@ describe('Users', () => {
         })
     })
 
-    it('should create a new user in database if user is created successfully', (done) => {
+    it('should create a new user in database if user is created successfully', function(done) {
       let user = {
         name: 'Another Person',
         email: 'person@another.com',
@@ -105,17 +111,17 @@ describe('Users', () => {
       chai.request(server)
         .post('/signup')
         .send(user)
-        .end((err, res) => {
+        .end(function(err, res) {
           res.should.have.status(200)
           res.body.should.be.a('object')
           res.body.message.should.equal('Verification email sent successfully')
           users = DB.getDB().collection('users')
           users.findOne({email: user.email})
-            .then((item) => {
+            .then(function(item) {
               item.name.should.equal(user.name)
               item.email.should.equal(user.email)
             })
-            .then(() => {
+            .then(function() {
               done()
             })
         })
@@ -124,22 +130,22 @@ describe('Users', () => {
   })
 
   // app.post('/resendverification',user.resendVerification)
-  describe('POST /resendverification', () => {
+  describe('POST /resendverification', function() {
     let user = {
       name: "Name",
       email: "person@example.com",
       verified: false
     }
 
-    before('sending verification email', (done) => {
-      DB.getDB().collection('users').insert(user, (err, res) => { done() })
+    before('sending verification email', function(done) {
+      DB.getDB().collection('users').insert(user, function(err, res) { done() })
     })
 
-    it('should send a verification email', (done) => {
+    it('should send a verification email', function(done) {
       chai.request(server)
         .post('/resendverification')
         .send({ email: user.email })
-        .end((err, res) => {
+        .end(function(err, res) {
           res.should.have.status(200)
           res.body.should.be.a('object')
           res.body.message.should.equal('Verification email sent successfully')
@@ -147,12 +153,12 @@ describe('Users', () => {
         })
     })
 
-    it('should give an error without email', (done) => {
+    it('should give an error without email', function(done) {
       let nothing = {}
       chai.request(server)
         .post('/resendverification')
         .send(nothing)
-        .end((err, res) => {
+        .end(function(err, res) {
           res.should.have.status(501)
           res.body.should.be.a('object')
           res.body.message.should.equal('Email missing')
@@ -162,21 +168,21 @@ describe('Users', () => {
   })
 
   // app.post('/verify-email',user.verify)
-  describe('POST /verify-email', () => {
+  describe('POST /verify-email', function() {
     let user = {
       email: "person@example.com",
       verify_token: "verificationtoken"
     }
-    before('verifying email', (done) => {
+    before('verifying email', function(done) {
       DB.connect(done)
       DB.getDB().collection('users').insert(user)
     })
 
-    it('should confirm the verification of a user', (done) => {
+    it('should confirm the verification of a user', function(done) {
       chai.request(server)
         .post('/verify-email')
         .send(user)
-        .end( (err, res) => {
+        .end( function(err, res) {
           res.should.have.status(200)
           res.body.message.should.equal('user verified successfully')
           done()
@@ -184,7 +190,7 @@ describe('Users', () => {
     })
   })
 
-  describe('Authenticated test cases', () => {
+  describe('Authenticated test cases', function() {
     var token
     var approved_user_id
     var disapproved_user_id
@@ -200,14 +206,14 @@ describe('Users', () => {
       pass_verify_token: "verifytoken"
     }
 
-    beforeEach('create and login the user', (done) => {
+    beforeEach('create and login the user', function(done) {
       console.log("Creating and logging in user")
       DB.getDB().collection('users').insert(user)
-        .then(() => {
+        .then(function() {
           chai.request(server)
             .post('/login')
             .send({ email: user.email, password: password })
-            .end( (err, res) => {
+            .end( function(err, res) {
               token = res.body.token
               done()
             })
@@ -215,12 +221,12 @@ describe('Users', () => {
     })
 
     // app.post('/login',user.login)
-    describe('POST /login', () => {
-      it('should log the user in', (done) => {
+    describe('POST /login', function() {
+      it('should log the user in', function(done) {
         chai.request(server)
           .post('/login')
           .send({ email: user.email, password: password })
-          .end( (err, res) => {
+          .end( function(err, res) {
             res.should.have.status(200)
             done()
           })
@@ -228,12 +234,12 @@ describe('Users', () => {
     })
 
     // app.post('/me',user.profile)
-    describe('POST /me', () => {
-      it('should retrieve profile details', (done) => {
+    describe('POST /me', function() {
+      it('should retrieve profile details', function(done) {
         chai.request(server)
           .post('/me')
           .set('Authorization', 'Bearer ' + token)
-          .end( (err, res) => {
+          .end( function(err, res) {
             res.should.have.status(200)
             done()
           })
@@ -241,27 +247,27 @@ describe('Users', () => {
     })
 
     // app.get('/users/:pageNo/:perPage',user.getUsers)
-    describe('POST /users/1/10', () => {
-      it('should paginate the users', (done) => {
+    describe('POST /users/1/10', function() {
+      it('should paginate the users', function(done) {
         done()
       })
     })
 
     // app.post('/users/approve',user.approve)
-    describe('POST /users/approve', () => {
+    describe('POST /users/approve', function() {
 
-      before('create an approvable user', (done) => {
+      before('create an approvable user', function(done) {
         let new_user = {
           email: 'admin@example.com',
           verified: true
         }
-        DB.getDB().collection('users').insertOne(new_user).then((item) => {
+        DB.getDB().collection('users').insertOne(new_user).then(function(item) {
           disapproved_user_id = item.insertedId
           done()
         })
       })
 
-      it('should approve the user', (done) => {
+      it('should approve the user', function(done) {
         let approve_payload = {
           approve_user_id: disapproved_user_id,
           email: user.email
@@ -270,7 +276,7 @@ describe('Users', () => {
           .post('/users/approve')
           .set('Authorization', 'Bearer ' + token)
           .send(approve_payload)
-          .end( (err, res) => {
+          .end( function(err, res) {
             res.should.have.status(200)
             done()
           })
@@ -278,21 +284,21 @@ describe('Users', () => {
     })
 
     // app.post('/users/disapprove',user.disapprove)
-    describe('POST /users/disapprove', () => {
+    describe('POST /users/disapprove', function() {
 
-      before('create an approved user', (done) => {
+      before('create an approved user', function(done) {
         let new_user = {
           email: 'admin@example.com',
           verified: true,
           approved: true
         }
-        DB.getDB().collection('users').insertOne(new_user).then((item) => {
+        DB.getDB().collection('users').insertOne(new_user).then(function(item) {
           approved_user_id = item.insertedId
           done()
         })
       })
 
-      it('should disapprove the user', (done) => {
+      it('should disapprove the user', function(done) {
         let disapprove_payload = {
           approve_user_id: approved_user_id,
           email: user.email
@@ -301,7 +307,7 @@ describe('Users', () => {
           .post('/users/disapprove')
           .set('Authorization', 'Bearer ' + token)
           .send(disapprove_payload)
-          .end( (err, res) => {
+          .end( function(err, res) {
             res.should.have.status(200)
             done()
           })
@@ -309,19 +315,19 @@ describe('Users', () => {
     })
 
     // app.post('/password/forgot',user.passwordForgot)
-    describe('POST /password/forgot', () => {
+    describe('POST /password/forgot', function() {
       let user = {
         email: 'person@example.com'
       }
-      before('sending email, create a user', (done) => {
+      before('sending email, create a user', function(done) {
         DB.connect(done)
         DB.getDB().collection('users').insertOne(user)
       })
-      it('should take the email', (done) => {
+      it('should take the email', function(done) {
         chai.request(server)
           .post('/password/forgot')
           .send(user)
-          .end( (err, res) => {
+          .end( function(err, res) {
             res.should.have.status(200)
             done()
           })
@@ -329,13 +335,13 @@ describe('Users', () => {
     })
 
     // app.post('/password/reset',user.passwordReset)
-    describe('POST /password/reset', () => {
+    describe('POST /password/reset', function() {
       let user = {
         email: 'person@example.com',
         pass_verify_timestamp: Math.floor(Date.now() / 1000),
         pass_verify_token: "verifytoken"
       }
-      it('should request a new password from the user', (done) => {
+      it('should request a new password from the user', function(done) {
         let reset_user = {
           email: 'person@example.com',
           pass_token: 'verifytoken',
@@ -344,7 +350,7 @@ describe('Users', () => {
         chai.request(server)
           .post('/password/reset')
           .send(reset_user)
-          .end( (err, res) => {
+          .end( function(err, res) {
             res.should.have.status(200)
             done()
           })
@@ -354,13 +360,13 @@ describe('Users', () => {
   })
 
   // app.post('/logout',user.logout)
-  describe('POST /logout', () => {
-    it('should log the user out', (done) => {
+  describe('POST /logout', function() {
+    it('should log the user out', function(done) {
       chai.request(server)
         .post('/logout')
         .set('Authorization', 'Bearer ' + token)
         .send({email: user.email})
-        .end( (err, res) => {
+        .end( function(err, res) {
           // TODO: Change back to
           // res.should.have.status(200)
           res.should.have.status(401)
@@ -370,13 +376,13 @@ describe('Users', () => {
   })
 
   // app.post('/password/change',user.passwordChange)
-  describe('POST /password/change', () => {
-    it('should change password of the user', (done) => {
+  describe('POST /password/change', function() {
+    it('should change password of the user', function(done) {
       done()
     })
   })
 
-  after('all tests, clear everything', (done) => {
+  after('all tests, clear everything', function(done) {
     DB.drop(done)
   })
 
